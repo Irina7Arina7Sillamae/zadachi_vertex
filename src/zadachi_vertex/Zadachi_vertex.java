cd81b171e34f2b1a438b88b40688f279a5824b42
irina Siskova <Irina.Siskova@ivkhk.ee>
2021-09-20 15:30:13 +0300

    задачи на циклы

---
+++ src/zadachi_vertex/Zadachi_vertex.java
@@ -0,0 +1,83 @@
+
+package zadachi_vertex;
+import java.util.Scanner;
+
+public class Zadachi_vertex {
+
+    
+    public static void main(String[] args) {
+        //ЗАДАЧИ от  Vertex
+        //Задача 1
+        //Вывести на экан числа от 1 до 5
+        
+        /*
+        for(int i = 1; i <= 5; i++ ) {
+            System.out.print(i + " " );
+        }
+        
+        //Задача 2
+        //Вывести на экан числа от 5 до 1
+        
+        for (int i = 5; i >= 1; i--){
+            System.out.print(i + " ");
+        }
+        
+        //Задача 3
+        //Вывести на экан таблицу умножения на 3
+        System.out.println("Таблица умножения на 3:");
+        for
+                ( int a = 1; a <= 10; a++ ) {
+            
+            System.out.println("3 * " + a + " = " + 3*a );
+        }
+        
+        for (int i = 1; i <= 7; i++ ) {
+            System.out.println(i + "я выполняюсь перед continue!");
+            if ( i > 5 )
+                continue;
+                System.out.println("                " + i + ": я выолняюсь всего " + i +  " раз(а), после continue ; ");
+        }
+
+        System.out.println("Таблица умножения на 3: " );
+        for (int i = 1; i <= 10; i++) {
+            if (i > 6) 
+                continue;
+            System.out.println( "3 * " + i + " = " + 3*i);
+            if (i < 6) 
+                continue;
+        }
+        
+        //Написать программу, которая суммирует все числа от 1 до введенного пользователем числа
+        
+         System.out.println("Введите любое целое положительное число: ");
+         Scanner nsc = new Scanner(System.in);
+         int num = nsc.nextInt();
+         int summ = 0;
+         for (int i = 1; i <= num; i++) {
+             summ = summ + i;
+         }
+         System.out.println(summ);
+        
+        
+        int a = 7;
+        while (a <= 98){
+            System.out.print(a + " ");
+            a += 7;
+        }
+          int b = 5;
+          while (b <= 100){
+              System.out.print(  b + " ");
+              b += 5;
+          } */
+        
+            int c = 2;
+            while(c <= 512){
+                System.out.println(c + "  ");
+                c *= 2;
+            }
+                    
+    
+         
+    }
+    
+}
